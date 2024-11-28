62fb641d52b3adc37d0a6bbb4f7900072a924220
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-11-28 15:37:01 +0530

    python modules and packages

---
+++ Day-04/test-modules/main.py
@@ -0,0 +1,14 @@
+# import my_module
+import math # importing math module
+# import math as m # importing math module with alias name m
+
+from my_module import add # importing add function from my_module
+
+# print(my_module.add(1, 2))
+
+print(add(1, 2))
+
+# value of pi
+print(math.pi)
+# factorial of a number
+print(math.factorial(5))
