802c931fcc772073b14714f85a453f6d1125b0e8
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-11-28 17:06:40 +0530

    module importing python

--- Day-04/my_project/main.py
+++ Day-04/my_project/main.py
@@ -1,21 +1,4 @@
+from my_calculator import add, subtract
 
-# import my_calculator.addition
-# import my_calculator.subtraction
-
-# print(my_calculator.addition.add(5, 3))
-# print(my_calculator.subtraction.subtract(5, 3))
-
-
-# #Import entire module
-# from my_calculator import addition
-# from my_calculator import subtraction
-
-# print(addition.add(5, 3))
-# print(subtraction.subtract(5, 3))
-
-# #Import specific function
-from my_calculator.addition import add
-from my_calculator.subtraction import subtract
-
-print(add(5, 3))
-print(subtract(5, 3))
+print(add(1, 2))
+print(subtract(2, 1))
---
+++ Day-04/my_project/my_calculator/__init__.py
@@ -0,0 +1,3 @@
+# without __init__.py file, python will not recognize the folder as a package, so we need to create __init__.py file
+from .addition import add
+from .subtraction import subtract
